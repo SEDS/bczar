d9c02d487244cadfa36e1c195327c67182e04678
hilljh82 <hilljh82@gmail.com>
2015-09-01 13:53:47 -0400

    Script was missing preamable; fixed THIS_DIR

--- build/commands/GenerateConfigCommand.py
+++ build/commands/GenerateConfigCommand.py
@@ -44,51 +44,52 @@ class GenerateConfigContext (Context):
 # Command that generates the configuration files for a workspace.
 #
 class GenerateConfigCommand (Command):  
-  context = GenerateConfigContext
+	context = GenerateConfigContext
+	
+	#
+	# Get the command's name
+	#
+	def name (self):
+		return 'genconfig'
+	
+	#
+	# Execute the command
+	#
+	def execute (self, ctx):
+		# Open the properties for the file.
+		properties_filename = path.join (ctx.prefix, 'configure.properties')
+		
+		from ..scripts import PropertiesFile
+		propfile = PropertiesFile.PropertiesFile ()
+		propfile.open (properties_filename)
+		
+		#
+		# Helper method that open the script file for the correct
+		# platform. This is important since different platforms have
+		# different ways of configuring their environment from the
+		# command-line.
+		#
+		def open_script_file (prefix):
+			from ..Utilities import is_windows_platform
+			
+			if is_windows_platform ():
+				# We are going to use a batch file.
+				from build.scripts import BatchFile
+				script = BatchFile.BatchFile ()        
+				script.open (path.join (prefix, 'configure.bat'))
+				
+			else:
+				from build.scripts import ShFile
+				script = ShFile.ShFile ()
+				script.open (path.join (prefix, 'configure.sh'))
+					
+			return script
 
-  #
-  # Get the command's name
-  #
-  def name (self):
-    return 'genconfig'
-
-  #
-  # Execute the command
-  #
-  def execute (self, ctx):
-    # Open the properties for the file.
-    properties_filename = path.join (ctx.prefix, 'configure.properties')
-    
-    from ..scripts import PropertiesFile
-    propfile = PropertiesFile.PropertiesFile ()
-    propfile.open (properties_filename)
-    
-    # Open the script file for writing.
-    script = open_script_file (ctx.prefix)
-    
-    for proj in ctx.workspace.get_projects ():
-      logging.getLogger ().info ('updating script for {0}...'.format (proj.name ()))
-      proj.update_script (ctx.prefix, propfile)
-      proj.update_script (ctx.prefix, script)  
-
-#
-# Helper method that open the script file for the correct
-# platform. This is important since different platforms have
-# different ways of configuring their environment from the
-# command-line.
-#
-def open_script_file (prefix):
-  from ..Utilities import is_windows_platform
-  
-  if is_windows_platform ():
-    # We are going to use a batch file.
-    from build.scripts import BatchFile
-    script = BatchFile.BatchFile ()        
-    script.open (path.join (prefix, 'configure.bat'))
-    
-  else:
-    from build.scripts import ShFile
-    script = ShFile.ShFile ()
-    script.open (path.join (prefix, 'configure.sh'))
-      
-  return script
+		# Open the script file for writing.
+		script = open_script_file (ctx.prefix)
+		script.write_preamble ()
+	
+		for proj in ctx.workspace.get_projects ():
+			logging.getLogger ().info ('updating script for {0}...'.format (proj.name ()))
+			proj.update_script (ctx.prefix, propfile)
+			proj.update_script (ctx.prefix, script)  
